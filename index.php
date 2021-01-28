142e7d36895c0c139b2d999e21edbed9f680fbda
Anna Rzucidło <arzucidlo1@gmail.com>
2021-01-28 12:03:17 +0100

    komit2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+#echo "Hi there";
 require 'Routing.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
@@ -8,9 +8,8 @@ $path = parse_url($path, PHP_URL_PATH);
 Router::get('', 'AdminController');
 Router::get('projects', 'BoardController');
 Router::post('login', 'SecurityController');
-//Router::post('addProject', 'BoardController');
+
 Router::post('register', 'SecurityController');
-//Router::post('search', 'BoardController');
 Router::get('like', 'BoardController');
 Router::get('dislike', 'BoardController');
 
